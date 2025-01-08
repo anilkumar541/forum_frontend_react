cf3aa4f96b32e1dc392102c09093814a08fb962a
anilkumar541 <anilec4870@gmail.com>
2025-01-08 15:49:26 +0530

    made button responsive

--- src/components/home_page_button_header/ButtonContainer.jsx
+++ src/components/home_page_button_header/ButtonContainer.jsx
@@ -1,36 +1,44 @@
+
 import React from 'react'
 import { Link } from 'react-router-dom';
 
 export const ButtonContainer = ({user, setIsModalOpen, searchTag, setSearchTag}) => {
   return (
-    <div className="flex justify-between mb-4 gap-3">
-        <input type="text" value={searchTag} onChange={(e)=> setSearchTag(e.target.value)} placeholder='search...' className='border border-slate-600 px-4 py-2 rounded'/>
+    <div className="flex flex-col sm:flex-row justify-between mb-4 gap-3">
+        <input 
+          type="text" 
+          value={searchTag} 
+          onChange={(e) => setSearchTag(e.target.value)} 
+          placeholder='search...' 
+          className='border border-slate-600 px-4 py-2 rounded w-full sm:w-auto' 
+        />
 
-        <div className='flex justify-end gap-3'>
-        <Link
-          to={user ? "/create-question" : "#"}
-          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
-          onClick={() => {
-            if (!user) {
-              setIsModalOpen(true);
-              return;
-            }
-          }}
-        >
-          Create Question
-        </Link>
-        <Link
-          to={user ? "/create-tag" : "#"}
-          className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
-          onClick={() => {
-            if (!user) {
-              setIsModalOpen(true);
-              return;
-            }
-          }}
-        >
-          Add New Tag
-        </Link>
+        <div className='flex flex-col sm:flex-row justify-end gap-3 w-full sm:w-auto'>
+          <Link
+            to={user ? "/create-question" : "#"}
+            className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 w-full sm:w-auto"
+            onClick={() => {
+              if (!user) {
+                setIsModalOpen(true);
+                return;
+              }
+            }}
+          >
+            Create Question
+          </Link>
+
+          <Link
+            to={user ? "/create-tag" : "#"}
+            className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 w-full sm:w-auto"
+            onClick={() => {
+              if (!user) {
+                setIsModalOpen(true);
+                return;
+              }
+            }}
+          >
+            Add New Tag
+          </Link>
         </div>
       </div>
   )
